40be9f35aa8cb9b98f169cd33dcb616d4a85299e
pl7x3cut0r <sec.username@proton.me>
2025-04-23 06:15:15 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
-
-# Installing Dependencies
-echo "Installing Dependencies..."
+# Installing Requirements
+echo "Setting up the required components, please wait..."
 sudo apt update
 sudo apt install git axel openjdk-21-jre openjdk-22-jre openjdk-23-jre -y
-
 # Cloning
-git clone https://github.com/xiv3r/Burpsuite-Professional.git 
+git clone https://github.com/pl7x3cut0r/Burpsuite-Professional.git
 cd Burpsuite-Professional
-
 # Download Burpsuite Professional
 echo "Downloading Burpsuite Professional Latest..."
 version=2025
 url="https://portswigger-cdn.net/burp/releases/download?product=pro&type=Jar"
 axel "$url" -o "burpsuite_pro_v$version.jar"
-
 # Execute Key Generator
 echo "Starting Key loader.jar..."
 (java -jar loader.jar) &
-
 # Execute Burpsuite Professional
 echo "Executing Burpsuite Professional..."
 echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v$version.jar &" > burpsuitepro
